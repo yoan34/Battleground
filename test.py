13913046bfe4ef0bd33055d12e704b3d3d75a249
Bousquet Yoan <bousquet_yoan11@yahoo.fr>
2020-07-08 10:26:21 +0200

    add requirements file

--- test.py
+++ test.py
@@ -1,42 +1,79 @@
-import time
+import time, os
 from Components.Player import Player
+from constants.minions import MINIONS_POOL
 
-player = Player('yoyo', isHuman=True)
-timing = 0 if player.isHuman else int(input('Enter a timing between each action [100-2000]: '))
-player.time_by_action = timing/1000
-player.shop = player.create_shop(1)
 
-total, force, team = 0, 0, ''
+
+# player = Player('yoyo', pool, is_human=False)
+
+# player.is_bot = False
+# player.lvl = 6
+# player.hero = player.get_hero()
+
+
+# a = time.time()
+
+# for _ in range(100000):
+    
+#     # if not player.is_bot:
+#     #     timing = 0 if player.is_human else int(input('Enter a timing between each action [100-2000]: '))
+#     # player.display_pool()
+#     player.create_shop(player.lvl)
+#     # print('\n\n')
+#     # player.display_pool()
+# b = time.time()
+# print(player.shop)
+# print(player.display_pool())
+
+# print(b-a)
+
+
+
+a = time.time()
 for i in range(1):
-    a = time.time()
-    player = Player('yoyo', isHuman=False)
-    # player.shop = player.create_minions(0, False, 'alleycat', 'alleycat', 'alleycat', 'alleycat')
-    player.shop = player.create_shop(1)
+    ###### SET UP TEST ######
+    pool = MINIONS_POOL.copy()
+    pool[2]['shifterZerus']['copy'] = 5000
+    player = Player('IA', pool, is_human=True)
+    player.debug = False
+    player.time_by_action = 1 if player.is_human else 0
+    player.hero = player.create_hero('George')
+    player.lvl = 3
+    player.gold = 10
+    print(i, '' ,player.hero.name)
+    player.create_shop(player.lvl)
+    player.hero.power['do'](player) if player.hero.power['trigger'] in ('turn', 'now') else None
+
+    
+    ##### MODE HUMAN #####
+    # player.shop = player.create_minions(0, False, 'alleycat', 'zoobot', 'zoobot', 'zoobot', 'zoobot', 'zoobot')
+    # player.board.extend(player.create_minions(0,True, 'brannBronzebeard'))
+    # player.brann = 1
+    # player.n_brann = 1
+    # player.n_khadgar = 2
+    # player.n_g_khadgar = 1
+    # player.gold = 99
+
+    ###### MODE IA ######
+    
+    ########################
     while True:
+        # player.history.append(str(player))
         print(player) if player.time_by_action else None
+        
         action = player.get_action()
         end = player.make(action)
+
         if end:
             break
-    b = time.time()
-    total += b-a
-    board_force = player.board_force()
-    if board_force > force:
-        force = board_force
-        team = player.__str__()
+    
 
-print(player)
-print(team)
-print(force)
-# print("average time by run: {}".format(total/1000))
-# print(' - - turn {} - -'.format(player.turn))
-# print('Total action: {} in {}'.format(player.n_action, round(b-a, 5)))
-# print('total buy: {}'.format(player.n_buy))
-# print('total play: {}'.format(player.n_play))
-# print('total sold: {} '.format(player.n_sold))
-# print('total swap: {}'.format(player.n_swap))
-# print('total up: {} '.format(player.n_up))
-# print('total refresh: {}'.format(player.n_refresh))
-# print('total freeze: {}'.format(player.n_freeze))
+print('\n\n'+str(player)) if not player.is_human else None
+
+b = time.time()
+print(player.n_action)
+print(b-a)
 
 
+
+# 1-t-t-t-t-t
